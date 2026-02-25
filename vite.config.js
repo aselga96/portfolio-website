8f5ced909cd5cbacca0678c905b6cc0290800a8f
aselga96 <aselga96@gmail.com>
2026-02-25 12:20:51 -0800

    Fix Vercel deployment issues - update vite.config.js base path for production

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   optimizeDeps: {
     exclude: ['@swc/core', 'lightningcss', '@vitejs/plugin-react-swc']
   },
